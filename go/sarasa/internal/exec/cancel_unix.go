23d8ce24a8075777183cfbe1e1a8e211669a3ce8
indrasvat <koffee2kode@gmail.com>
2026-07-02 15:31:29 -0700

    fix: keep timeout escalation for child process groups

--- go/sarasa/internal/exec/cancel_unix.go
+++ go/sarasa/internal/exec/cancel_unix.go
@@ -13,18 +13,31 @@ import (
 func configureCancel(cmd *exec.Cmd) func() {
 	done := make(chan struct{})
 	var doneOnce sync.Once
+	var mu sync.Mutex
+	var pgid int
+	var canceled bool
 	markDone := func() {
-		doneOnce.Do(func() {
-			close(done)
-		})
+		mu.Lock()
+		cancelWasSent := canceled
+		cancelPGID := pgid
+		mu.Unlock()
+		if !cancelWasSent || !processGroupExists(cancelPGID) {
+			doneOnce.Do(func() {
+				close(done)
+			})
+		}
 	}
 
 	cmd.Cancel = func() error {
 		if cmd.Process == nil {
 			return os.ErrProcessDone
 		}
-		pgid := cmd.Process.Pid
-		if err := syscall.Kill(-pgid, syscall.SIGINT); err != nil && err != syscall.ESRCH {
+		cancelPGID := cmd.Process.Pid
+		mu.Lock()
+		pgid = cancelPGID
+		canceled = true
+		mu.Unlock()
+		if err := syscall.Kill(-cancelPGID, syscall.SIGINT); err != nil && err != syscall.ESRCH {
 			return err
 		}
 		go func() {
@@ -34,11 +47,21 @@ func configureCancel(cmd *exec.Cmd) func() {
 			case <-done:
 				return
 			case <-timer.C:
-				_ = syscall.Kill(-pgid, syscall.SIGKILL)
+				if processGroupExists(cancelPGID) {
+					_ = syscall.Kill(-cancelPGID, syscall.SIGKILL)
+				}
 			}
 		}()
 		return nil
 	}
 	cmd.WaitDelay = 5 * time.Second
 	return markDone
+}
+
+func processGroupExists(pgid int) bool {
+	if pgid <= 0 {
+		return false
+	}
+	err := syscall.Kill(-pgid, 0)
+	return err == nil || err == syscall.EPERM
 }
